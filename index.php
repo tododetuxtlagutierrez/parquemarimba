1e1c926e77b631ff396701b5238167c98409130b
luis <desarollador.web.tuxtla@gmail.com>
2021-04-27 11:25:03 -0500

    casi listo

--- index.php
+++ index.php
@@ -9,15 +9,65 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 </head>
 <body>
-    
-    <div class="jumbotron jumbotron-fluid" style="background-image: url(https://images.unsplash.com/photo-1552152974-19b9caf99137?fit=crop&w=1350&q=80);">
+    <header>
+      <div class="jumbotron jumbotron-fluid bg-dark">
         <div class="container">
           <h1 class="display-4 text-white">Parque de la marimba</h1>
           <p class="lead text-white">Parque ubicado en Tuxtla Gutiérrez, Chiapas.</p>
-          <p class="text-white">Encuentra toda la información que necesites sobre el parque y </p>
+          <p class="text-white">Un lugar para disfrutar de un buen baile al ritmo de la marimba</p>
         </div>
       </div>
-      <br>
+
+    </header>
+    
+    <div class="container">
+      <div class="row mb-4">
+        <div class="col-12 col-lg-6">
+          <img src="./img/parque_marimba_tuxtla_gutierrez.jpg" class="img-fluid" alt="Ubicación del parque de la marimba en Tuxtla Gutiérrez">
+        </div>
+        <div class="col-12 col-lg-6">
+
+          <p class="mt-4">
+            El parque de la marimba es uno de los lugares preferidos de los tuxtlecos y chiapanecos, y con justa razón, 
+            es un lugar muy bello y agradable, ideal para tomar un descanso o pasear con tu familia y seres queridos.
+          </p>
+          <p>Es un lugar perfecto para escuchar la tradicional marimba, la música tradicional por excelencia en Chiapas,
+            ya que en este estado la marimba es un instrumento emblemático.
+          </p>
+          <p>El parque tiene una estructura de tipo colonial y cuenta con muchas bancas en donde descansar y sentarse, en el centro se encuentra el kiosco, 
+            en donde se reúnen para tocar las hermosas melodías de la marimba.
+          </p>
+
+        </div>
+      </div>
+
+      <h2 class="mb-4">Horarios del parque de la marimba</h2>
+      <img src="./img/horarios_parque_marimba_tuxtla_gutierrez.jpg" class="img-fluid" alt="Horarios del parque de la marimba en Tuxtla Gutiérrez">
+      <p class="mt-4">No debes preocuparte por el horario, ya que está abierto las 24 horas.</p>
+      <p>Pero lo mejor empieza en la tarde, más o menos a partir de las 6 de la tarde hasta las 9 de la noche.</p>
+      <p>En la tarde noche empiezan a tocar la marimba y las parejas empiezan a sacar sus mejores pasos y disfrutar
+        de un buen baile con su pareja.
+      </p>
+
+      <h2 class="mb-4">¿Dónde está el parque de la marimba?</h2>
+      <img src="./img/ubicacion_parque_marimba_tuxtla_gutierrez.jpg" class="img-fluid" alt="Ubicación del parque de la marimba en Tuxtla Gutiérrez">
+
+      <p class="mt-4"> La ubicación del parque de la marimba es en la dirección: avenida central entre octava y novena poniente, enfrente de una farmacia del ahorro.</p>
+
+      <h2>Historia del parque de la marimba</h2>
+
+      <p>Este precioso lugar se fundó el 12 de septiembre del año 1993 por el gobernador Elmar Zetser Gutiérrez.</p>
+      <p>Y ¿cómo llego la marimba a Chiapas?</p>
+      <p>Hay muchas versiones, pero la más contada es que:</p>
+      <p>Un africano trajo consigo la marimba y que en San Cristóbal De Las Casas empezaría a darse a conocer
+        las melodías de este instrumento.
+      </p>
+      <p>Pero es Corazón Borraz quien crear la primera marimba 100% chiapaneca, y con el cual empieza 
+        las melodías que hoy conocemos. 
+      </p>
+      <p>La marimba se fabrica el árbol de Hormiguillo, el cual se encuentra en los bosques altos de Chiapas.</p>
+    </div>
+      
       
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
